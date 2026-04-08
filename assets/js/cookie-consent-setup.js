f0643baf54930a3997b0754557951bd9e821b6b2
marteczkah <48037981+marteczkah@users.noreply.github.com>
2026-04-08 17:47:31 +0000

    Deploying to gh-pages from @ marteczkah/marteczkah.github.io@27790d2704eb0ff9f424cd432ec5cb77523c3f3b 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://alshedivat.github.io/al-folio/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://marteczkah.github.iohe subpath of your site, e.g. /blog/. Leave blank for root/#contact">contact us</a>.'
               }
             ]
           }
