ca820bf90b431047e8fde1e936c81a395b5fe212
marteczkah <48037981+marteczkah@users.noreply.github.com>
2026-04-09 16:25:27 +0000

    Deploying to gh-pages from @ marteczkah/marteczkah.github.io@c388e83d549629b5aaf8f532d81f369fbd96c108 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,21 +16,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
@@ -65,6 +51,12 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "news-2-papers-accepted-at-wacv2026",
           title: '2 papers accepted at WACV2026!',
+          description: "",
+          section: "News",},{id: "news-i-m-co-organizing-a-workshop-on-medical-foundation-models-at-haicon26",
+          title: 'I’m co-organizing a workshop on medical foundation models at HAICON26!',
+          description: "",
+          section: "News",},{id: "news-i-m-co-organizing-a-multitab-workshop-at-miccai26",
+          title: 'I’m co-organizing a MultiTab workshop at MICCAI26!',
           description: "",
           section: "News",},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
